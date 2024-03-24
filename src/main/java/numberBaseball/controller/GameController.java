0d06564c0b6c38fc139713d84c511017032cb07b
junohpark221 <junohpark221@gmail.com>
2024-03-25 00:49:39 +0900

    feat: 게임 시작 후 컴퓨터 플레이어의 난수 생성 기능 추가

---
+++ src/main/java/numberBaseball/controller/GameController.java
@@ -0,0 +1,13 @@
+package numberBaseball.controller;
+
+import numberBaseball.model.ComputerPlayer;
+
+public class GameController {
+    public void startGame() {
+        playGame();
+    }
+
+    public void playGame() {
+        ComputerPlayer computerPlayer = new ComputerPlayer();
+    }
+}
